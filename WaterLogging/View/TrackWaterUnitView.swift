deab9e9d3f0f415f8f91d07a0efad6703415d499
xiaoxuewang728 <xw88@duke.edu>
2020-06-16 01:35:45 -0700

    build UI on trackWater tab

---
+++ WaterLogging/View/TrackWaterUnitView.swift
@@ -0,0 +1,57 @@
+//
+//  TrackWaterUnitView.swift
+//  WaterLogging
+//
+//  Created by WangXiaoxue on 6/16/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackWaterUnitView: UIView {
+    private let headerLabel = UILabel()
+    private let contentLabel = UILabel()
+    init(header: String, content: String) {
+        super.init(frame: CGRect.zero)
+        headerLabel.text = header
+        contentLabel.text = content
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(headerLabel)
+        addSubview(contentLabel)
+
+        headerLabel.font = UIFont.systemFont(ofSize: 20.0)
+        headerLabel.numberOfLines = 0
+        headerLabel.textAlignment = .center
+        contentLabel.font = UIFont.systemFont(ofSize: 30.0)
+        contentLabel.numberOfLines = 0
+        contentLabel.textAlignment = .center
+
+        let headerLabelConstraints = [headerLabel.topAnchor.constraint(equalTo: self.topAnchor),
+                                      headerLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                                      headerLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)]
+
+        NSLayoutConstraint.activate(headerLabelConstraints)
+
+        let contentLabelConstraints = [contentLabel.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20),
+                                       contentLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                                       contentLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                                       contentLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)]
+
+        NSLayoutConstraint.activate(contentLabelConstraints)
+    }
+
+    func updateContent(text: String) {
+        contentLabel.text = text
+    }
+}
+
--- WaterLogging/View/TrackWaterViewController.swift
+++ WaterLogging/View/TrackWaterViewController.swift
@@ -11,7 +11,11 @@ protocol TrackWaterView: class {}
 class TrackWaterViewController: UIViewController, TrackWaterView {
     
     private var presenter: TrackWaterViewPresenter!
+    private let dateLabel = UILabel()
+    private let quantityUnitView = TrackWaterUnitView(header: "Consumed", content: "0 oz")
+    private let goalUnitView = TrackWaterUnitView(header: "Your Goal", content: "n/a")
     private let addWaterButton = UIButton()
+    private let instructionLabel = UILabel()
     private let updateGoalButton = UIButton()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -27,12 +31,22 @@ class TrackWaterViewController: UIViewController, TrackWaterView {
         super.viewDidLoad()
         presenter = TrackWaterPresenter(view: self)
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        dateLabel.text = Date().toString()
+    }
+
     // Set Up
     
     private func setUp() {
+        dateLabel.text = Date().toString()
         addWaterButton.setTitle("Add 8 oz Water", for: .normal)
         updateGoalButton.setTitle("Update Daily Goal", for: .normal)
         addWaterButton.addTarget(self, action: #selector(addWaterButtonPressed), for: .touchUpInside)
+        instructionLabel.text = "Please allow this app to read weight from health in order to calculate daily goal based on weight"
+        instructionLabel.numberOfLines = 0
+        instructionLabel.textAlignment = .center
         updateGoalButton.addTarget(self, action: #selector(goalButtonPressed), for: .touchUpInside)
         
         view.backgroundColor = .systemBackground
@@ -44,36 +58,81 @@ class TrackWaterViewController: UIViewController, TrackWaterView {
     
     private func setUpConstraints() {
         let container = UIView()
+
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        container.addSubview(dateLabel)
+
+        let unitViewContainer = UIView()
+        unitViewContainer.translatesAutoresizingMaskIntoConstraints = false
+        quantityUnitView.translatesAutoresizingMaskIntoConstraints = false
+        goalUnitView.translatesAutoresizingMaskIntoConstraints = false
+        unitViewContainer.addSubview(quantityUnitView)
+        unitViewContainer.addSubview(goalUnitView)
+        container.addSubview(unitViewContainer)
+
+        // unit views constraints
+        let quantityUnitViewConstraints = [quantityUnitView.topAnchor.constraint(equalTo: unitViewContainer.topAnchor),
+                                           quantityUnitView.leadingAnchor.constraint(equalTo: unitViewContainer.leadingAnchor),
+                                           quantityUnitView.bottomAnchor.constraint(equalTo: unitViewContainer.bottomAnchor)]
+
+        NSLayoutConstraint.activate(quantityUnitViewConstraints)
+
+        let goalUnitViewConstraints = [goalUnitView.topAnchor.constraint(equalTo: unitViewContainer.topAnchor),
+                                       goalUnitView.leadingAnchor.constraint(equalTo: quantityUnitView.trailingAnchor, constant: 50),
+                                       goalUnitView.trailingAnchor.constraint(equalTo: unitViewContainer.trailingAnchor),
+                                       goalUnitView.bottomAnchor.constraint(equalTo: unitViewContainer.bottomAnchor)]
+
+        NSLayoutConstraint.activate(goalUnitViewConstraints)
+
         addWaterButton.translatesAutoresizingMaskIntoConstraints = false
+        instructionLabel.translatesAutoresizingMaskIntoConstraints = false
         updateGoalButton.translatesAutoresizingMaskIntoConstraints = false
         container.addSubview(addWaterButton)
+        container.addSubview(instructionLabel)
         container.addSubview(updateGoalButton)
         container.translatesAutoresizingMaskIntoConstraints = false
-        
+
         view.addSubview(container)
-        
+
+        // dateLabel constraints
+        let dateLabelConstraints = [dateLabel.topAnchor.constraint(equalTo: container.topAnchor),
+                                    dateLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor)]
+
+        NSLayoutConstraint.activate(dateLabelConstraints)
+
+        // unitViewContainer constraints
+        let unitViewContainerConstraints = [unitViewContainer.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 50),
+                                            unitViewContainer.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+                                            unitViewContainer.trailingAnchor.constraint(equalTo: container.trailingAnchor)]
+
+        NSLayoutConstraint.activate(unitViewContainerConstraints)
+
         // Buttons constraints
-        let addWaterButtonConstraints = [addWaterButton.topAnchor.constraint(equalTo: container.topAnchor),
+        let addWaterButtonConstraints = [addWaterButton.topAnchor.constraint(equalTo: unitViewContainer.bottomAnchor, constant: 50),
                                          addWaterButton.leadingAnchor.constraint(equalTo: container.leadingAnchor),
                                          addWaterButton.trailingAnchor.constraint(equalTo: container.trailingAnchor),]
-        
+
         NSLayoutConstraint.activate(addWaterButtonConstraints)
-        
-        let updateGoalButtonConstraints = [updateGoalButton.topAnchor.constraint(equalTo: addWaterButton.bottomAnchor, constant: 10),
+
+        let instructionLabelConstraints = [instructionLabel.topAnchor.constraint(equalTo: addWaterButton.bottomAnchor, constant: 10),
+                                           instructionLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+                                           instructionLabel.widthAnchor.constraint(equalToConstant: 300)]
+
+        NSLayoutConstraint.activate(instructionLabelConstraints)
+
+        let updateGoalButtonConstraints = [updateGoalButton.topAnchor.constraint(equalTo: instructionLabel.bottomAnchor, constant: 10),
                                            updateGoalButton.leadingAnchor.constraint(equalTo: container.leadingAnchor),
                                            updateGoalButton.trailingAnchor.constraint(equalTo: container.trailingAnchor),
                                            updateGoalButton.bottomAnchor.constraint(equalTo: container.bottomAnchor)]
-        
+
         NSLayoutConstraint.activate(updateGoalButtonConstraints)
-        
+
         // ContainerView constraints
         
         let containerConstraints = [container.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-                                    container.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-                                    container.topAnchor.constraint(greaterThanOrEqualTo: self.view.topAnchor),
+                                    container.topAnchor.constraint(greaterThanOrEqualTo: self.view.topAnchor, constant: 100),
                                     container.leadingAnchor.constraint(greaterThanOrEqualTo: self.view.leadingAnchor),
-                                    container.trailingAnchor.constraint(lessThanOrEqualTo: self.view.trailingAnchor),
-                                    container.bottomAnchor.constraint(lessThanOrEqualTo: self.view.bottomAnchor)]
+                                    container.trailingAnchor.constraint(lessThanOrEqualTo: self.view.trailingAnchor)]
         
         NSLayoutConstraint.activate(containerConstraints)
         
@@ -92,3 +151,11 @@ class TrackWaterViewController: UIViewController, TrackWaterView {
 
 }
 
+extension Date {
+    func toString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = TimeZone.current
+        return dateFormatter.string(from: self)
+    }
+}
