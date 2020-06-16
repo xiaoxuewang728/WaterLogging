1aea6323ad552131e475c7c8cf53b80330eb8397
xiaoxuewang728 <xw88@duke.edu>
2020-06-16 05:06:50 -0700

    cite sources

--- WaterLogging/View/ProgressView.swift
+++ WaterLogging/View/ProgressView.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 
+// source: https://github.com/MarcosiOSdev/DrinkingWater/blob/master/DrinkingWater/CounterView.swift
 class ProgressView: UIView {
 
     private struct Constants {
